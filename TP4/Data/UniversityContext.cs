b231daa998ddee090bedd864f5472292612e99b9
USER <USER@DESKTOP-E8JOF5O>
2022-12-19 14:43:37 +0100

    Ajoutez des fichiers projet.

---
+++ TP4/Data/UniversityContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TP4.Models;
+namespace TP4.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+        static private UniversityContext universityContextInstance = null;
+        static public int count = 0;
+        private UniversityContext(DbContextOptions o) : base(o)
+        {
+            count++;
+        }
+        static public UniversityContext Instantiate_UniversityContext()
+        {
+            if (universityContextInstance == null)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+                optionsBuilder.UseSqlite(@"Data Source=C:\2022 GL3 .NET Framework TP4 - SQLite database.db");
+                universityContextInstance = new UniversityContext(optionsBuilder.Options);
+                return universityContextInstance;
+            }
+            else
+            {
+                return universityContextInstance;
+            }
+        }
+    }
+}
